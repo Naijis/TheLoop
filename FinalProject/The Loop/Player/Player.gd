69674bc29e814f5d443caffb3aa397fe620e8cb3
Naijis <164218976+Naijis@users.noreply.github.com>
2024-03-20 09:46:52 -0700

    Add files via upload

---
+++ FinalProject/The Loop/Player/Player.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+	
+func _physics_process(delta):
+	# Add the gravity.
+	var directionY = Input.get_axis("ui_up", "ui_down")
+	if directionY:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var directionX = Input.get_axis("ui_left", "ui_right")
+	if directionX == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	else :
+		get_node("AnimatedSprite2D").flip_h = false
+	if directionX:
+		velocity.x = directionX * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
